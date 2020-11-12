ac005bfb43e5e4581481c2b53c16872fee11239b
Jianrui Hu <u0886750@utah.edum>
2020-11-11 21:13:10 -0700

    navigate bar

---
+++ src/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { NavLink, Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    const restaurantLinks = this.props.restaurants.map(r => (
+      <li className='nav-item' key={r.name}>
+        <NavLink exact to={`/${r.name}`} className='nav-link'>
+          {r.name}
+        </NavLink>
+      </li>
+    ));
+    return (
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <Link className='navbar-brand' to='/'>
+          UEat
+        </Link>
+
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon' />
+        </button>
+
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            {restaurantLinks}
+            <li className='nav-item'>
+              <NavLink exact to='/signin' className='nav-link'>
+                Sign In
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+    );
+  }
+}
+export default Navbar;
