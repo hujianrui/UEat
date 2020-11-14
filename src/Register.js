fd55c9b54c0af7ac48d2061257c2bf08759c250f
Jianrui Hu <u0886750@utah.edum>
2020-11-13 20:16:37 -0700

    update register page

--- src/Register.js
+++ src/Register.js
@@ -11,16 +11,6 @@ class Register extends Component {
                     <h3>Register</h3>
 
                     <div className="form-group">
-                        <label>First name</label>
-                        <input type="text" className="form-control" placeholder="First name" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Last name</label>
-                        <input type="text" className="form-control" placeholder="Last name" />
-                    </div>
-
-                    <div className="form-group">
                         <label>Email</label>
                         <input type="email" className="form-control" placeholder="Enter email" />
                     </div>
@@ -28,6 +18,16 @@ class Register extends Component {
                     <div className="form-group">
                         <label>Password</label>
                         <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Confirm Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Invite Code</label>
+                        <input type="text" className="form-control" placeholder="Enter invite code" />
                     </div>
 
                     <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
